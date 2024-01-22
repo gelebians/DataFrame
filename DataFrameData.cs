9a352ef2926eb849bbc544f04ca5a3641ebc75df
gelebians <iciftci.35@hotmail.com>
2024-01-22 12:30:15 +0300

    Update DataFrameData.cs

--- DataFrameData.cs
+++ DataFrameData.cs
@@ -1,15 +1,32 @@
+using ISRA.System;
 using System;
-using System.Linq;
-using System.Runtime.InteropServices;
-using Technical.System;
 
-namespace Technical
+namespace ISRA.Data
 {
+    /*
+     * This application is for implementing pandas dataframe in c#.
+     * There are DataFrameData constructor in the DataFrameData.cs file.
+     * This library was designed by PoSeYDoN.
+     */
+    /// <summary>
+    /// Data class used for DataFrame
+    /// </summary>
     public partial class DataFrameData
     {
         Array _data;
+        /// <summary>
+        /// Array of values
+        /// </summary>
         public Array Values => _data;
+        /// <summary>
+        /// DataFrameData type of data of the data
+        /// </summary>
         public Type Type { get; set; }
+        /// <summary>
+        /// Create by specifying type and size
+        /// </summary>
+        /// <param name="type"> data type</param>
+        /// <param name="size"> datas size</param>
         public DataFrameData(Type type,int size)
         {
             Type = type;
@@ -28,8 +45,14 @@ namespace Technical
             else if (Type == typeof(string))
                 _data = new string?[size];
             else
-                throw new NotImplementedException("Unrecognized data type:("+Type.Name+") DataFrameData(Type type,int size))
+                General.Logging.Error("Unrecognized data type:(" + Type.Name + ") DataFrameData(Type type,int size)");
         }
+        
+        /// <summary>
+        /// Creating DataFrameData with array
+        /// </summary>
+        /// <param name="data">Array of data</param>
+        /// <exception cref="NotImplementedException"></exception>
         public DataFrameData(Array data)
         {
             _data = data;
@@ -45,6 +68,11 @@ namespace Technical
                 _ => throw new NotImplementedException("Unrecognized data type")
             };
         }
+        /// <summary>
+        /// Add new data
+        /// </summary>
+        /// <param name="data">Data to add</param>
+        /// <exception cref="NotImplementedException">Data type error</exception>
         public void Add(IConvertible? data)
         {
             _data = _data switch
@@ -77,31 +105,39 @@ namespace Technical
             }
             else
             {
-                throw new Exception("Index was outside the specified range.: DataFrameData.Add(int index,IConvertible? data)");
+                General.Logging.Error("Index was outside the specified range.: DataFrameData.Add(int index,IConvertible? data)");
             }
             
         }
+        /// <summary>
+        /// Deleting data given with index number
+        /// </summary>
+        /// <param name="index"> index number</param>
+        /// <exception cref="NotImplementedException"></exception>
         public void Delete(int index)
         {
             if (index >= 0 && index < Count())
             {
                 _data = _data switch
                 {
-                    bool?[] bool1 => General.DeleteArray(bool1,index),
-                    int?[] int1 => General.DeleteArray(int1, index),
-                    float?[] float1 => General.DeleteArray(float1, index),
-                    double?[] double1 => General.DeleteArray(double1, index),
-                    decimal?[] decimal1 => General.DeleteArray(decimal1, index),
-                    DateTime?[] dateTime1 => General.DeleteArray(dateTime1, index),
-                    string?[] string1 => General.DeleteArray(string1, index),
+                    bool?[] bool1 => General.RemoveArray(bool1,index),
+                    int?[] int1 => General.RemoveArray(int1, index),
+                    float?[] float1 => General.RemoveArray(float1, index),
+                    double?[] double1 => General.RemoveArray(double1, index),
+                    decimal?[] decimal1 => General.RemoveArray(decimal1, index),
+                    DateTime?[] dateTime1 => General.RemoveArray(dateTime1, index),
+                    string?[] string1 => General.RemoveArray(string1, index),
                     _ => throw new NotImplementedException("Unrecognized data type")
                 };
             }
             else
             {
-                throw new Exception("Index was outside the specified range.: DataFrameData.Add(int index,IConvertible? data)");
+                General.Logging.Error("Index was outside the specified range.: DataFrameData.Add(int index,IConvertible? data)");
             }
         }
+        /// <summary>
+        /// Clear all data
+        /// </summary>
         public void Clear()
         {
             if (Type == typeof(bool))
@@ -119,12 +155,17 @@ namespace Technical
             else if (Type == typeof(string))
                 _data = new string?[Count()];
             else
-                General.Loglayıcı.Error("Unrecognized data type:(" + Type.Name + ") DataFrameData.Clear()");
+                General.Logging.Error("Unrecognized data type:(" + Type.Name + ") DataFrameData.Clear()");
         }
+        /// <summary>
+        /// Converts data to string in tabular form
+        /// </summary>
+        /// <returns> string</returns>
+        /// <exception cref="NotImplementedException"></exception>
         public override string ToString()
         {
-            string str = "DataType:" + Type.Name+" Size:"+Count()+"\nDatas:";
-            str+=_data switch
+            string str = "DataType:" + Type.Name + " Size:" + Count() + "\nDatas:";
+            str += _data switch
             {
                 bool?[] bool1 => string.Join(",", bool1),
                 int?[] int1 => string.Join(",", int1),
@@ -137,38 +178,55 @@ namespace Technical
             };
             return str;
         }
-        public DataFrameData ShiftLeft(int count=1)
+        /// <summary>
+        /// Shifts the array to the left by the given number. Initial values ​​are assigned as null
+        /// </summary>
+        /// <param name="counter"> number to shift left</param>
+        /// <returns>DataFrameData</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public DataFrameData ShiftLeft(int counter=1)
         {
             Array data = _data switch
             {
-                bool?[] bool1 => General.Join(new bool?[count], bool1.Take(Count() - count).ToArray()),
-                int?[] int1 => General.Join(new int?[count],int1.Take(Count() - count).ToArray()),
-                float?[] float1 => General.Join(new float?[count], float1.Take(Count() - count).ToArray()),
-                double?[] double1 => General.Join(new double?[count], double1.Take(Count() - count).ToArray()),
-                decimal?[] decimal1 => General.Join(new decimal?[count], decimal1.Take(Count() - count).ToArray()),
-                DateTime?[] dateTime1 => General.Join(new DateTime?[count], dateTime1.Take(Count() - count).ToArray()),
-                string?[] string1 => General.Join(new string?[count], string1.Take(Count() - count).ToArray()),
+                bool?[] bool1 => General.ArrayJoin(new bool?[counter], bool1.Take(Count() - counter).ToArray()),
+                int?[] int1 => General.ArrayJoin(new int?[counter],int1.Take(Count() - counter).ToArray()),
+                float?[] float1 => General.ArrayJoin(new float?[counter], float1.Take(Count() - counter).ToArray()),
+                double?[] double1 => General.ArrayJoin(new double?[counter], double1.Take(Count() - counter).ToArray()),
+                decimal?[] decimal1 => General.ArrayJoin(new decimal?[counter], decimal1.Take(Count() - counter).ToArray()),
+                DateTime?[] dateTime1 => General.ArrayJoin(new DateTime?[counter], dateTime1.Take(Count() - counter).ToArray()),
+                string?[] string1 => General.ArrayJoin(new string?[counter], string1.Take(Count() - counter).ToArray()),
                 _ => throw new NotImplementedException("Unrecognized data type")
             };
 
             return new DataFrameData(data);
         }
-        public DataFrameData ShiftRight(int count = 1)
+        /// <summary>
+        /// Shifts the array to the right by the given number. Initial values ​​are assigned as null
+        /// </summary>
+        /// <param name="counter">number to shift right</param>
+        /// <returns>DataFrameData</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public DataFrameData ShiftRight(int counter = 1)
         {
             Array data = _data switch
             {
-                bool?[] bool1 => General.Join(bool1.Skip(count).Take(Count() - count).ToArray(), new bool?[count]),
-                int?[] int1 => General.Join(int1.Skip(count).Take(Count() - count).ToArray(), new int?[count]),
-                float?[] float1 => General.Join(float1.Skip(count).Take(Count() - count).ToArray(), new float?[count]),
-                double?[] double1 => General.Join(double1.Skip(count).Take(Count() - count).ToArray(), new double?[count]),
-                decimal?[] decimal1 => General.Join(decimal1.Skip(count).Take(Count() - count).ToArray(), new decimal?[count]),
-                DateTime?[] dateTime1 => General.Join(dateTime1.Skip(count).Take(Count() - count).ToArray(), new DateTime?[count]),
-                string?[] string1 => General.Join(string1.Skip(count).Take(Count() - count).ToArray(), new string?[count]),
+                bool?[] bool1 => General.ArrayJoin(bool1.Skip(counter).Take(Count() - counter).ToArray(), new bool?[counter]),
+                int?[] int1 => General.ArrayJoin(int1.Skip(counter).Take(Count() - counter).ToArray(), new int?[counter]),
+                float?[] float1 => General.ArrayJoin(float1.Skip(counter).Take(Count() - counter).ToArray(), new float?[counter]),
+                double?[] double1 => General.ArrayJoin(double1.Skip(counter).Take(Count() - counter).ToArray(), new double?[counter]),
+                decimal?[] decimal1 => General.ArrayJoin(decimal1.Skip(counter).Take(Count() - counter).ToArray(), new decimal?[counter]),
+                DateTime?[] dateTime1 => General.ArrayJoin(dateTime1.Skip(counter).Take(Count() - counter).ToArray(), new DateTime?[counter]),
+                string?[] string1 => General.ArrayJoin(string1.Skip(counter).Take(Count() - counter).ToArray(), new string?[counter]),
                 _ => throw new NotImplementedException("Unrecognized data type")
             };
 
             return new DataFrameData(data);
         }
+        /// <summary>
+        /// Returns the index number of the given data
+        /// </summary>
+        /// <param name="data">Data to be indexed</param>
+        /// <returns>int</returns>
         public int index(IConvertible? data)
         {
             int indx = _data switch
@@ -184,11 +242,15 @@ namespace Technical
             };
             return indx;
         }
+        /// <summary>
+        /// Foreach method used in the list method
+        /// </summary>
+        /// <param name="action">Method to run</param>
         public void ForEach(Action<IConvertible?> action)
         {
             if (action == null)
             {
-                throw new Exception("action cannot be left blank");
+                General.Logging.Error("Index was outside the specified range.: DataFrameData.ForEach(Action<T>)");
             }
 
             for (int i = 0; i < Count(); i++)
@@ -196,5 +258,20 @@ namespace Technical
                 action(this[i]);
             }
         }
+        public DataFrameData Short(ShortSide side=ShortSide.Asc)
+        {
+            return _data switch
+            {
+                bool?[] bool1 => new DataFrameData((side == ShortSide.Asc) ? bool1.OrderBy(x => x).ToArray() : bool1.OrderByDescending(x => x).ToArray()),
+                int?[] int1 => new DataFrameData((side == ShortSide.Asc) ? int1.OrderBy(x => x).ToArray() : int1.OrderByDescending(x => x).ToArray()),
+                float?[] float1 => new DataFrameData((side == ShortSide.Asc) ? float1.OrderBy(x => x).ToArray() : float1.OrderByDescending(x => x).ToArray()),
+                double?[] double1 => new DataFrameData((side == ShortSide.Asc) ? double1.OrderBy(x => x).ToArray() : double1.OrderByDescending(x => x).ToArray()),
+                decimal?[] decimal1 => new DataFrameData((side == ShortSide.Asc) ? decimal1.OrderBy(x => x).ToArray() : decimal1.OrderByDescending(x => x).ToArray()),
+                DateTime?[] dateTime1 => new DataFrameData((side == ShortSide.Asc) ? dateTime1.OrderBy(x => x).ToArray() : dateTime1.OrderByDescending(x => x).ToArray()),
+                string?[] string1 => new DataFrameData((side == ShortSide.Asc) ? string1.OrderBy(x => x).ToArray() : string1.OrderByDescending(x => x).ToArray()),
+                _ => throw new NotImplementedException("Unrecognized data type")
+            };
+        }
+        
     }
 }
